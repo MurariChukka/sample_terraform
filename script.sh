7cc3fd7e0f6579a76a37d5374d13344e189330e5
MurariChukka <88134162+MurariChukka@users.noreply.github.com>
2024-09-02 22:34:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+# Update the package index
 sudo yum update -y
+
+# Install Apache web server
 sudo yum install -y httpd
+
+# Start the Apache service
 sudo systemctl start httpd
+
+# Enable Apache to start on boot
 sudo systemctl enable httpd
+
+# Copy index.html to the web server directory
+sudo cp /home/ec2-user/sample_terraform/index.html /var/www/html/
